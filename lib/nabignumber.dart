841ac130795c06f7ebf5570c89febbc3284bdae6
Nasser Amini <namini40@gmail.com>
2022-08-02 17:48:50 +0430

    rename module

--- lib/nabignumber.dart
+++ lib/nabignumber.dart
@@ -3,6 +3,6 @@
 /// More dartdocs go here.
 library bignumber;
 
-export 'src/bignumber_base.dart';
+export 'src/nabignumber_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
