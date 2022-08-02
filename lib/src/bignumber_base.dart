dc5db57ff5ba2783304a8614c667900752ae7b2f
Nasser Amini <namini40@gmail.com>
2022-08-02 15:56:59 +0430

    basic contructor added

--- lib/src/bignumber_base.dart
+++ lib/src/bignumber_base.dart
@@ -1,6 +1,17 @@
-// TODO: Put public facing types in this file.
-
-/// Checks if you are awesome. Spoiler: you are.
 class BigNumber {
-  bool get isAwesome => true;
+  BigInt _value = BigInt.zero;
+  int _numberOfFractionDigits = 0;
+
+  BigNumber({required int intPart, required int fractionPart}) {
+    _value = _concatIntParts2BigInt(intPart, fractionPart);
+    _numberOfFractionDigits = _numberIntOfDigits(fractionPart);
+  }
+
+  BigInt _concatIntParts2BigInt(int intPart, int fracPart) {
+    return BigInt.parse(intPart.toString() + fracPart.toString());
+  }
+
+  static int _numberIntOfDigits(int num) {
+    return num.toString().length;
+  }
 }
